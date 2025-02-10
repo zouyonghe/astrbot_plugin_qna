fc8f5c693ce2e9eb1100f9c18e91c97915c6b2ba
邹永赫 <1259085392@qq.com>
2025-02-11 00:42:03 +0900

    test

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ class QNA(Star):
 
         if question_keyword_list:
             self.question_pattern = r"(?i)(" + "|".join(map(re.escape, question_keyword_list)) + r")"
-            logger.debug(f"自动问答关键词正则: {self.question_pattern}")
 
     def _in_qna_group_list(self, event: AstrMessageEvent) -> bool:
         qna_group_list = [
